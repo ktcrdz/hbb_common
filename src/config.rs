5974d6348b24ad0248605320e8c17459043caf6e
ktcrdz <80935683@qq.com>
2025-06-17 12:32:27 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -101,8 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-
+"14.103.169.147"];
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 pub const WS_RENDEZVOUS_PORT: i32 = 21118;
